3f80fe2cb13a4a11a7209f38c69f72effc663cf5
hiiam-andy <andrew.citb@gmail.com>
2023-09-24 16:19:49 +0300

    фикс бага, поправил стили

--- src/entities/postCard/PostCard.js
+++ src/entities/postCard/PostCard.js
@@ -6,19 +6,18 @@ import styles from "./PostCard.module.css";
 export default function PostCard({ number, title, body }) {
   return (
     <div className={styles.card}>
-      <div className={styles.card_content}>
-        <div className={styles.card_heading}>
-          <div className={styles.card_number}>{number}.</div>
-          <h1 className={styles.card_title}>
-            <b>Заголовок: </b>
-            {title}
-          </h1>
-        </div>
-        <div className={styles.card_description}>
-          <b>Описание: </b>
-          <span className={styles.card_description_text}>{body}</span>
-        </div>
+      <div className={styles.card_heading}>
+        <div className={styles.card_number}>{number}.</div>
+        <h1 className={styles.card_title}>
+          <b>Заголовок: </b>
+          {title}
+        </h1>
       </div>
+      <div className={styles.card_description}>
+        <b>Описание: </b>
+        <span className={styles.card_description_text}>{body}</span>
+      </div>
+
       <NavLink to={`/${number}`} className={styles.card_btn}>
         <MyButton>Просмотр</MyButton>
       </NavLink>
